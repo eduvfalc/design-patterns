8a690ba45e04422d43350ed79039a74a90c19e09
Eduardo Vieira <eduardo.vieira.falcao@gmail.com>
2023-08-03 21:43:56 +0200

    Change pointer type, fix output logged

--- abstract-factory/src/main.cpp
+++ abstract-factory/src/main.cpp
@@ -4,12 +4,12 @@
 
 int main () {
     std::cout << "Requesting kitchen furniture..." << std::endl;
-    std::shared_ptr<KitchenFactory> kitchenFactory {std::make_shared<KitchenFactory>()};
+    std::shared_ptr<IAbstractFactory> kitchenFactory {std::make_shared<KitchenFactory>()};
     std::shared_ptr<FurnitureClient> kitchenFurnitureClient {std::make_shared<FurnitureClient>(kitchenFactory)};
     kitchenFurnitureClient->CreateFurniture();
 
     std::cout << "Requesting office furniture..." << std::endl;
-    std::shared_ptr<OfficeFactory> officeFactory {std::make_shared<OfficeFactory>()};
+    std::shared_ptr<IAbstractFactory> officeFactory {std::make_shared<OfficeFactory>()};
     std::shared_ptr<FurnitureClient> officeFurnitureClient {std::make_shared<FurnitureClient>(officeFactory)};
     officeFurnitureClient->CreateFurniture();
 
